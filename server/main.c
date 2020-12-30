868a12982b94c5eba8e544f43d42a6d04ba6ccbe
EdwarDaaboul <edwardaaboul@mail.tau.ac.il>
2020-12-30 15:21:20 +0200

    .

--- server/main.c
+++ server/main.c
@@ -6,8 +6,14 @@
 #include <ws2tcpip.h>
 #pragma comment(lib, "Ws2_32.lib")
 
+#define SERVER_ADDRESS_STR "127.0.0.1"
+
 void Main_server()
 {
+ SOCKET MainSocket = INVALID_SOCKET;
+ unsigned long Address;
+ SOCKADDR_IN service;
+
 // Initialize Winsock.
 WSADATA wsaData;
 int StartupRes = WSAStartup(MAKEWORD(2, 2), &wsaData);
@@ -20,4 +26,36 @@ if (StartupRes != NO_ERROR)
 }
 
 /* The WinSock DLL is acceptable. Proceed. */
+
+// Create a socket.    
+MainSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+if (MainSocket == INVALID_SOCKET)
+{
+    printf("Error at socket( ): %ld\n", WSAGetLastError());
+    goto server_cleanup_1;
+}
+
+// Create a sockaddr_in object and set its values.
+	// Declare variables
+
+Address = inet_addr(SERVER_ADDRESS_STR); 
+if (Address == INADDR_NONE)
+{
+	printf("The string \"%s\" cannot be converted into an ip address. ending program.\n",
+		SERVER_ADDRESS_STR);
+	goto server_cleanup_2;
+}
+
+service.sin_family = AF_INET;
+service.sin_addr.s_addr = Address;
+service.sin_port = htons(SERVER_PORT); //The htons function converts a u_short from host to TCP/IP network byte order 
+								   //( which is big-endian ).
+server_cleanup_2:
+if (closesocket(MainSocket) == SOCKET_ERROR)
+printf("Failed to close MainSocket, error %ld. Ending program\n", WSAGetLastError());
+
+server_cleanup_1:
+if (WSACleanup() == SOCKET_ERROR)
+printf("Failed to close Winsocket, error %ld. Ending program.\n", WSAGetLastError());
 }
